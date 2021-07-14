b741a9603ea81fe8c07594a7c18e3ad49a917790
DoM4 <c0cc0dr1ll080>
2021-07-14 13:53:04 +0100

    ALign dependancies across every module

--- gradlePlugins/src/main/kotlin/com/domenico/aumenta/multimodulemvvm/gradle/DependenciesPlugin.kt
+++ gradlePlugins/src/main/kotlin/com/domenico/aumenta/multimodulemvvm/gradle/DependenciesPlugin.kt
@@ -9,6 +9,14 @@ class DependenciesPlugin : Plugin<Project> {
 
     companion object {
         val koinVersion = "3.0.2"
-        val kotlin_version = "1.5.21"
+        val kotlinVersion = "1.5.21"
+        val coreKtlVersion = "1.6.0"
+        val appCompactVersion = "1.3.0"
+        val materialVersion = "1.4.0"
+        val constraintLayoutVersion = "2.0.4"
+        val navigationKtlVersion = "2.3.5"
+        val testExtJunitVersion = "1.1.3"
+        val espressoCoreVersion = "3.4.0"
+        val JUnitVersion = "4.13.2"
     }
 }
